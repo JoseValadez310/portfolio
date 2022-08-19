115cc039c7d3f00ed38c25071cefdabc39ce792c
Jose <josevaladez310@gmail.com>
2022-08-18 20:33:34 -0700

    Home article got a 5 sections     - welcome     - Story         - an introduction animation taking 100vh was created     - Works         - N/A     - Resume         - N/A     - Contact         - N/A

--- src/Tabs/Home.js
+++ src/Tabs/Home.js
@@ -1,10 +1,41 @@
+import '../Styles/Home.css'
+
 const Home = () => {
     return (
-        <section className="home-section">
-            <h1>
-                Home section
-            </h1> 
-        </section>
+        <article className="home-section">
+
+            <section  className = 'hero-wrapper'> 
+                <h1 className = 'main-text'>My name is Jose Valadez</h1>    
+                <p className = 'sub-text'> Welcome To My Portfolio.</p>   
+            </section>
+
+            <section className = "story-overlook">
+                <h1>
+                    Story
+                </h1>
+            </section>
+
+            <section className = "works-overlook">
+                <h1>
+                    works
+                </h1>
+            </section>
+
+            <section className = "resume-overlook">
+                <h1>
+                    resume
+                </h1>
+            </section>
+
+            <section className = "contact">
+                <h1>
+                    contact
+                </h1>
+            </section>
+
+        </article>
+
+       
     )
 }
 
