eec0d91e1075bab1800d1fe50c57ac33d4475fd2
Jose <josevaladez310@gmail.com>
2022-08-20 15:16:53 -0700

    Home page update     - React-Intersection-Observer was added     - This will allow for custom animations for intersecting viewports

--- src/Tabs/Home.js
+++ src/Tabs/Home.js
@@ -1,15 +1,55 @@
 import '../Styles/Home.css'
+import Nav from '../Components/Nav';
+import { useRef, useState, useEffect } from 'react'
+ import { useInView } from 'react-intersection-observer';
+
 
 const Home = () => {
+
+    
+        const {ref: story , inView: storyView } = useInView()
+            console.log(storyView)
+    
+        const {ref: contact , inView: contactView} = useInView()
+            console.log(contactView)
+        
+   
+    
+
+   
+    
+
+
+
+
+// without the use of react-intersection
+// const ref = useRef(); 
+// const [elementVisiable, setElementVisiable] = useState();
+//     useEffect(()=>{
+//         const observer = new IntersectionObserver((entries) => {
+//             const entry = entries[0]
+//             setElementVisiable(entry.isIntersecting)
+//         })
+//         observer.observe(ref.current)
+//     },[])    
+// console.log(elementVisiable)
+
+
+
+
+
+
+
+
+
     return (
         <article className="home-section">
-
-            <section  className = 'hero-wrapper'> 
+            <section className = 'hero-wrapper'> 
                 <h1 className = 'main-text'>My name is Jose Valadez</h1>    
                 <p className = 'sub-text'> Welcome To My Portfolio.</p>   
             </section>
 
-            <section className = "story-overlook">
+            <section className = "story-overlook" ref = {story}>
                 <h1>
                     Story
                 </h1>
@@ -19,12 +59,11 @@ const Home = () => {
                 <p>
                     I simply thought that was the only way until I soon figured out that getting a hands on experince was worth than the picture itself. My mindset took a 180 and since then, I've created my major project with many other ideas to come.   
                 </p>
-
             </section>
 
             <section className = "works-overlook">
                 <h1>
-                    works
+                    {`Works has entred view `}
                 </h1>
             </section>
 
@@ -34,9 +73,9 @@ const Home = () => {
                 </h1>
             </section>
 
-            <section className = "contact">
+            <section  className = "contact" ref={contact}>
                 <h1>
-                    contact
+                    {  `Contact has entred view `}
                 </h1>
             </section>
 
